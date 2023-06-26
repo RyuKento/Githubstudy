20fa9fe496a25df8d8b3934c26f48077b2fde679
RyuKento <83810925+RyuKento@users.noreply.github.com>
2023-06-26 16:51:03 +0900

    sixth commit

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
-// JavaScript code
-const pushListElement = document.getElementById("pushList");
+const fetch = require("node-fetch");
+const fs = require("fs");
+
+const pushListElement = "<ul>";
 
 async function fetchPushes() {
   const response = await fetch("https://api.github.com/repos/RyuKento/Githubstudy/events");
@@ -13,27 +15,28 @@ async function fetchPushes() {
         const commitDate = new Date(event.created_at);
         const fileName = commitDate.toISOString().slice(0, 19).replace(/[-T:]/g, "") + ".md";
 
-        const listItem = document.createElement("li");
-        listItem.textContent = commitMessage;
-        pushListElement.appendChild(listItem);
+        pushListElement += `<li>${commitMessage}</li>`;
 
         const fileContent = `# Commit Message\n\n${commitMessage}\n\n`;
         createFile(fileName, fileContent);
       }
     }
   }
+
+  pushListElement += "</ul>";
+  fs.writeFileSync("index.html", pushListElement);
 }
 
 async function createFile(fileName, content) {
   const response = await fetch("https://api.github.com/repos/RyuKento/Githubstudy/contents/" + fileName, {
     method: "PUT",
     headers: {
-      Authorization: "Bearer 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",
+      Authorization: "Bearer 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",
       Accept: "application/vnd.github.v3+json",
     },
     body: JSON.stringify({
       message: "Create " + fileName,
-      content: btoa(content),
+      content: Buffer.from(content).toString("base64"),
     }),
   });
 
